5e9e9de6918718a602b09ec1be56fea0b9e7027b
x4e554c4c <wanderingdwarf03@gmail.com>
2020-07-29 22:52:42 +0300

    Added services layout

---
+++ app/Services/AudioService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+class AudioService
+{
+
+    public function create()
+    {
+
+    }
+
+    public function get()
+    {
+        
+    }
+
+    public function all()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
---
+++ app/Services/VideoService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+class VideoService
+{
+
+    public function create()
+    {
+
+    }
+
+    public function get()
+    {
+        
+    }
+
+    public function all()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
