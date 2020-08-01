dbc9e542652df0ed26ca1c30f3d7cefe5a92261c
Crybaka <kryptolone@protonmail.com>
2020-08-01 18:52:01 +0300

    blade integration

---
+++ resources/views/sections/basement.blade.php
@@ -0,0 +1,55 @@
+<section class="basement">
+  <footer class="footer container">
+    <div class="footer__wrapper footer__wrapper--first">
+      <h5 class="footer__title heading-fifth">Contacts</h5>
+      <div class="footer__inner">
+        <div class="footer__segment footer__segment--first">
+          <div class="footer__element">
+            <i class="footer__icon fab fa-youtube"></i>
+            <a href="#" class="footer__text text-fourth">youtube</a>
+          </div>
+          <div class="footer__element">
+            <i class="footer__icon fab fa-twitter"></i>
+            <a href="#" class="footer__text text-fourth">@twetter</a>
+          </div>
+          <div class="footer__element footer__element--last">
+            <i class="footer__icon fab fa-facebook"></i>
+            <a href="#" class="footer__text text-fourth">@facebook</a>
+          </div>
+        </div>
+        <div class="footer__segment footer__segment--second">
+          <div class="footer__element">
+            <i class="footer__icon fas fa-envelope"></i>
+            <a href="#" class="footer__text text-fourth">mailadress@mail.com</a>
+          </div>
+          <div class="footer__element">
+            <i class="footer__icon fab fa-discord"></i>
+            <a href="#" class="footer__text text-fourth">discord#3221</a>
+          </div>
+          <div class="footer__element footer__element--last">
+            <i class="footer__icon fab fa-skype"></i>
+            <a href="#" class="footer__text text-fourth">@skype</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer__wrapper">
+      <div class="footer__inner">
+        <h5 class="footer__title heading-fifth">links</h5>
+        <div class="footer__segment footer__segment--first">
+          <a href="#" class="footer__link footer__text text-fourth">about me</a>
+          <a href="#" class="footer__link footer__text text-fourth">video</a>
+          <a href="#" class="footer__link footer__link--last footer__text text-fourth">audio</a>
+        </div>
+        <div class="footer__segment">
+          <a href="#" class="footer__link footer__text text-fourth">images</a>
+          <a href="#" class="footer__link footer__text text-fourth">games</a>
+          <a href="#" class="footer__link footer__link--last footer__text text-fourth">i recomend</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <div class="copyright container">
+    <p class="copyright__text">© Copyright 2020 *namehere* − All Rights Reserved</p>
+  </div>
+</section>
---
+++ resources/views/sections/navbar.blade.php
@@ -0,0 +1,40 @@
+<section class="navbar container">
+  <div class="navbar-desktop">
+    <nav class="navbar-desktop__links">
+      <a href="#" class="navbar-desktop__logo">Namehere</a>
+      <a href="#" class="navbar-desktop__link navbar-desktop__link--active">about me</a>
+      <a href="#" class="navbar-desktop__link">video</a>
+      <a href="#" class="navbar-desktop__link">audio</a>
+      <a href="#" class="navbar-desktop__link">images</a>
+      <a href="#" class="navbar-desktop__link">games</a>
+      <a href="#" class="navbar-desktop__link navbar-desktop__link--last">i recomend</a>
+    </nav>
+    <div class="navbar-desktop__aside">
+      <a href="#" class="navbar-desktop__aside-link navbar-desktop__aside-link--login-in">login in</a>
+      <a href="#" class="navbar-desktop__aside-link navbar-desktop__aside-link--sign-up">sing up</a>
+      <!-- <a href="#" class="navbar-desktop__account-link"><span class="material-icons-round">account_circle</span></a> -->
+    </div>
+  </div>
+
+  <div class="navbar-mobile">
+    <button class="navbar-mobile__button navbar-mobile__button--show"><span class="material-icons-round">menu</span></button>
+    <a href="#" class="navbar-mobile__logo">Namehere</a>
+    <nav class="navbar-mobile__links">
+      <div class="navbar-mobile__header">
+        <!-- <a href="#" class="navbar-mobile__account-link"><span class="material-icons-round">account_circle</span></a> -->
+        <button class="navbar-mobile__button navbar-mobile__button--hide"><span class="material-icons-round">clear</span></button>
+      </div>
+      <a href="#" class="navbar-mobile__link navbar-mobile__link--first navbar-mobile__link--active">about me</a>
+      <a href="#" class="navbar-mobile__link">video</a>
+      <a href="#" class="navbar-mobile__link">audio</a>
+      <a href="#" class="navbar-mobile__link">images</a>
+      <a href="#" class="navbar-mobile__link">games</a>
+      <a href="#" class="navbar-mobile__link">i recomend</a>
+      <div class="navbar-mobile__footer">
+        <a href="#" class="navbar-mobile__footer-link navbar-mobile__footer-link--login-in">login in</a>
+        <a href="#" class="navbar-mobile__footer-link navbar-mobile__footer-link--sign-up">sing up</a>
+      </div>
+    </nav>
+    <div class="navbar-mobile__overlay"></div>
+  </div>
+</section>
