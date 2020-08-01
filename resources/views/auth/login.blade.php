43aec4f90a6ba4294919e08b2b750572b82d658e
x4e554c4c <wanderingdwarf03@gmail.com>
2020-08-02 01:49:28 +0300

    Added login/register functionality to production code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,19 +8,20 @@
 
 @section('content')
   <section class="login-in container">
-    <form class="auth">
+    <form class="auth" method="POST" action="{{ route('login') }}">
+      @csrf
       <h4 class="auth__title heading-fourth">login in</h4>
       <div class="auth__input-group">
-        <input class="auth__input input-main" type="email" placeholder="your email">
+        <input class="auth__input input-main"  name="email" type="email" placeholder="your email" required>
         <span class="auth__input-icon auth__input-icon--type material-icons-round">email</span>
         <small class="auth__input-error"></small>
       </div>
       <div class="auth__input-group">
-        <input class="auth__input input-main" type="password" placeholder="repeat password">
+        <input class="auth__input input-main" name="password" type="password" placeholder="repeat password" required>
         <span class="auth__input-icon auth__input-icon--type material-icons-round">lock</span>
         <small class="auth__input-error"></small>
       </div>
-      <button class="auth__input-button button-main">confirm</button>
+      <button class="auth__input-button button-main" type="submit">confirm</button>
       <div class="login-with">
         <p class="login-with__text text-fourth">Or login with</p>
         <div class="login-with__buttons">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,34 +8,35 @@
 
 @section('content')
   <section class="sign-up container">
-    <form class="auth">
+    <form class="auth" method="POST" action="{{ route('register') }}">
+      @csrf
       <h4 class="auth__title heading-fourth">Sing up</h4>
       <div class="auth__input-group">
-        <input class="auth__input input-main" type="text" placeholder="your name">
+        <input class="auth__input input-main" type="text" name="name" placeholder="your name" required>
         <span class="auth__input-icon auth__input-icon--type material-icons-round">account_circle</span>
         <span class="auth__input-icon auth__input-icon--alarm material-icons-round">new_releases</span>
         <small class="auth__input-error">Your error here</small>
       </div>
       <div class="auth__input-group">
-        <input class="auth__input input-main" type="email" placeholder="your email">
+        <input class="auth__input input-main" type="email" name="email" placeholder="your email" required>
         <span class="auth__input-icon auth__input-icon--type material-icons-round">email</span>
         <span class="auth__input-icon auth__input-icon--success material-icons-round">verified</span>
         <small class="auth__input-error"></small>
       </div>
       <div class="auth__input-group">
-        <input class="auth__input input-main" type="password" placeholder="your password">
+        <input class="auth__input input-main" type="password" name="password" placeholder="your password" required>
         <span class="auth__input-icon auth__input-icon--visibility material-icons-round">visibility</span>
         <span class="auth__input-icon auth__input-icon--type material-icons-round">lock</span>
         <span class="auth__input-icon material-icons-round"></span>
         <small class="auth__input-error"></small>
       </div>
       <div class="auth__input-group">
-        <input class="auth__input input-main" type="password" placeholder="repeat password">
+        <input class="auth__input input-main" type="password" name="password_confirmation" placeholder="repeat password" required>
         <span class="auth__input-icon auth__input-icon--type material-icons-round">lock</span>
         <span class="auth__input-icon material-icons-round"></span>
         <small class="auth__input-error"></small>
       </div>
-      <button class="auth__input-button button-main">confirm</button>
+      <button class="auth__input-button button-main" type="submit">confirm</button>
       <div class="login-with">
         <p class="login-with__text text-fourth">Or login with</p>
         <div class="login-with__buttons">
