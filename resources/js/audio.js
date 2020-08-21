45b328a87df470b74679ae99e158b89a1f740ec6
x4e554c4c <wanderingdwarf03@gmail.com>
2020-08-21 23:47:08 +0300

    Added templates for base audio components (Vue)

--- resources/js/audio.js
+++ resources/js/audio.js
@@ -1,53 +1,34 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
- 
+// Importing Vue core
 import Vue from 'vue';
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-
+// Importing plugins
 import vClickOutside from 'v-click-outside'
-import VideoDetails from  '@components/content/video/VideoDetails';
-import VideoPlayer from  '@components/content/video/VideoPlayer';
+
+// Importing models
+import Audio from '@models/Videos'
+import Posts from '@models/Posts';
+
+// Importing  static components
 import ContextMenu from '@components/ContextMenu'
 import FilterBar from '@components/content/FilterBar'
 import Pool from  '@components/Pool';
-import Videos from '@models/Videos'
-import Posts from '@models/Posts';
+
+// Importing dynamic components
+import AudioDetails from  '@components/content/audio/AudioDetails';
+import AudioPlayer from  '@components/content/audio/AudioPlayer';
 
 
-Posts.self.track(Videos)
+Posts.self.track(Audio)
 
-Vue.use(vClickOutside)
 Vue.config.devtools = true;
+Vue.use(vClickOutside);
 
 const app = new Vue({
     el: document.querySelector('main'),
 
     components: {
-		VideoDetails,
-    	VideoPlayer,
+		AudioDetails,
+    	AudioPlayer,
 		FilterBar,
 		Pool,
 
--- resources/js/components/content/audio/AudioPlayer.vue
+++ resources/js/components/content/audio/AudioPlayer.vue
@@ -1,19 +1,41 @@
 <template>
 	<div class="player">
-    	<div class="player__rationed">
-  			<iframe class="player__video"
-			  		frameborder="0"
-					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-			  		:src="'https://www.youtube.com/embed/' + videoID + '?enablejsapi=1&color=white'"
-					allowfullscreen>
-			</iframe>
-			<transition name="fade">
-				<div v-if="showOverlay" class="player__overlay">
-      				<object class="player__overlay-image" :data="overlayUrl" type="image/svg+xml"></object>
-    			</div>
-			</transition>
-    	</div>
-	</div>
+        <div class="player__rationed">
+            <div class="player__image"></div>
+                <!--  <div class="player__overlay">
+                  <object class="player__overlay-image" data="{{ asset("img/svg/audio-overlay.svg") }}"
+                    type="image/svg+xml"></object>
+                </div>  -->
+            <div class="audio">
+                <div class="audio__player">
+                    <button class="audio__button-toggle"><i class="material-icons">visibility</i><span>show</span></button>
+                <div class="audio__progress-control">
+                    <button class="audio__button-state"><i class="fas fa-play"></i></button>
+                    <div class="audio__progress-bar">
+                        <div class="audio__progress-current">
+                            <button class="audio__progress-cursor"></button>
+                        </div>
+                        <div class="audio__progress-maximum"></div>
+                    </div>
+                    <time class="audio__timer">
+                        <span class="audio__timer-current">0:00</span>
+                        <span class="audio__timer-separator">/</span>
+                        <span class="audio__timer-maximum">0:00</span>
+                    </time>
+                </div>
+                <div class="audio__volume-control">
+                    <button class="audio__volume-button"><span class="material-icons-round">volume_up</span></button>
+                        <div class="audio__volume-bar">
+                            <div class="audio__volume-current">
+                                <button class="audio__volume-cursor"></button>
+                            </div>
+                            <div class="audio__volume-maximum"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
