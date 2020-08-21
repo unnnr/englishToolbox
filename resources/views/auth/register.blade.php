25614f9dbd37efb92c329efc5202dea7e3a35430
Crybaka <kryptolone@protonmail.com>
2020-08-21 18:18:21 +0300

    registration and login pages completed(html,css)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,51 +11,40 @@
     <form class="auth" method="POST" action="{{ route('register') }}">
       @csrf
       <h4 class="auth__title heading-fourth">Sing up</h4>
-      <div class="auth__input-group">
+      <div class="auth__input-group auth__input-group--account">
         <input class="auth__input input-main" type="text" name="name" placeholder="your name" required>
-        <span class="auth__input-icon auth__input-icon--type material-icons-round">account_circle</span>
-        <span class="auth__input-icon auth__input-icon--alarm material-icons-round">new_releases</span>
-        <small class="auth__input-error">Your error here</small>
+        <small class="auth__input-error"></small>
       </div>
-      <div class="auth__input-group">
+      <div class="auth__input-group auth__input-group--email auth__input-group--success">
         <input class="auth__input input-main" type="email" name="email" placeholder="your email" required>
-        <span class="auth__input-icon auth__input-icon--type material-icons-round">email</span>
-        <span class="auth__input-icon auth__input-icon--success material-icons-round">verified</span>
         <small class="auth__input-error"></small>
       </div>
-      <div class="auth__input-group">
+      <div class="auth__input-group auth__input-group--password">
         <input class="auth__input input-main" type="password" name="password" placeholder="your password" required>
-        <span class="auth__input-icon auth__input-icon--visibility material-icons-round">visibility</span>
-        <span class="auth__input-icon auth__input-icon--type material-icons-round">lock</span>
-        <span class="auth__input-icon material-icons-round"></span>
+        <button class="auth__input-visibility-button material-icons-round" type="button">visibility</button>
         <small class="auth__input-error"></small>
       </div>
-      <div class="auth__input-group">
+      <div class="auth__input-group auth__input-group--password">
         <input class="auth__input input-main" type="password" name="password_confirmation" placeholder="repeat password" required>
-        <span class="auth__input-icon auth__input-icon--type material-icons-round">lock</span>
-        <span class="auth__input-icon material-icons-round"></span>
         <small class="auth__input-error"></small>
       </div>
       <button class="auth__input-button button-main" type="submit">confirm</button>
       <div class="login-with">
         <p class="login-with__text text-fourth">Or login with</p>
         <div class="login-with__buttons">
-          <a href="#" class="login-with__button login-with__button--first"><i
-              class="login-with__icon login-with__icon--google fab fa-google"></i></a>
-          <a href="#" class="login-with__button login-with__button--second"><i
-              class="login-with__icon login-with__icon--facebook fab fa-facebook"></i></a>
-          <a href="#" class="login-with__button login-with__button--third"><i
-              class="login-with__icon login-with__icon--twetter fab fa-twitter"></i></a>
+          <button class="login-with__button" type="button"><i class="login-with__icon login-with__icon--google fab fa-google"></i></button>
+          <button class="login-with__button " type="button"><i class="login-with__icon login-with__icon--facebook fab fa-facebook"></i></button>
+          <button class="login-with__button" type="button"><i class="login-with__icon login-with__icon--twetter fab fa-twitter"></i></button>
         </div>
         <a href="#" class="login-with__link text-fourth">Already have an account?</a>
       </div>
     </form>
     <div class="form__poster">
-      <object class="form__img" data="/public/img/svg/signup.svg" type="image/svg+xml"></object>
+      <object class="form__img" data="{{ asset("img/svg/register.svg") }}" type="image/svg+xml"></object>
     </div>
   </section>
 
-  <section class="register-overlay">
+  {{-- <section class="register-overlay">
     <object class="register-overlay__image" data="{{ asset("img/svg/register-overlay.svg") }}" type="image/svg+xml"></object>
-  </section>
+  </section> --}}
 @endsection
