ad010bd36575bdf4f702e8e14269a204c5e5d7c6
x4e554c4c <wanderingdwarf03@gmail.com>
2020-08-30 13:59:18 +0300

    Removed deprecated blade files

--- resources/views/components/videoPlayer.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="player">
-  <div class="player__rationed">
-    <iframe class="player__video" src="https://www.youtube.com/embed/G1IbRujko-A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    {{-- <div class="player__overlay">
-      <object class="player__overlay-image" data="{{ asset("img/svg/video-overlay.svg") }}"
-        type="image/svg+xml"></object>
-    </div> --}}
-  </div>
-</div>
