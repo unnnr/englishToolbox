4aa5b2424c511e9e2353ba0dad671d7d7f8162d9
x4e554c4c <wanderingdwarf03@gmail.com>
2020-10-02 16:35:39 +0300

    Added animation on page changing (Vue)

--- app/src/App.vue
+++ app/src/App.vue
@@ -6,7 +6,9 @@
 
     <navbar/>
     
-    <router-view/>
+    <transition name="slide-right" mode="out-in">
+      <router-view/>
+    </transition>
 
     <basement/>
   </div>
@@ -40,9 +42,27 @@ export default {
 }
 </script>
 
-<style>
-  #app {
-    background: #fafafa;
-  }
+<style scoped>
+#app {
+  background: #fafafa;
+}
+
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition-duration: 0.4s;
+  transition-property: height, opacity, transform;
+  transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
+  overflow: hidden;
+}
+.slide-left-enter,
+.slide-right-leave-active {
+  opacity: 0;
+  transform: translate(2em, 0);
+}
+
+.slide-right-enter {
+  opacity: 0;
+  transform: translate(-2em, 0);
+}
 </style>
 
