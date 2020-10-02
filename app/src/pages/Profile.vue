ee66f9a8cbfb2780446d872c0296aa3953260371
Crybaka <kryptolone@protonmail.com>
2020-10-02 14:52:58 +0300

    banned section

--- app/src/pages/Profile.vue
+++ app/src/pages/Profile.vue
@@ -16,6 +16,74 @@
 
     <require-email-overlay/>
 
+    <section class="banned container">
+      <h3 class="heading-third">Banned users</h3>
+      <div class="banned__body">
+
+        <div class="banned__comment">
+          <div class="comment">
+            <div class="comment__image"></div>
+            <div class="comment__body">
+                <p class="comment__text text-sixth">
+                  <span class="comment__name">Eden Pepe</span>
+                  <span class="comment__mention">Za lupa</span>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr
+                </p>
+                <time class="comment__date text-sixth">13.09.2020</time>
+            </div>
+          </div>
+          <button class="banned__button button-second">Unban</button>
+        </div>
+
+        <div class="banned__comment">
+          <div class="comment">
+            <div class="comment__image"></div>
+            <div class="comment__body">
+                <p class="comment__text text-sixth">
+                  <span class="comment__name">Eden Pepe</span>
+                  <span class="comment__mention">Za lupa</span>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr
+                </p>
+                <time class="comment__date text-sixth">13.09.2020</time>
+            </div>
+          </div>
+          <button class="banned__button button-second">Unban</button>
+        </div>
+
+        <div class="banned__comment">
+          <div class="comment">
+            <div class="comment__image"></div>
+            <div class="comment__body">
+                <p class="comment__text text-sixth">
+                  <span class="comment__name">Eden Pepe</span>
+                  <span class="comment__mention">Za lupa</span>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr
+                </p>
+                <time class="comment__date text-sixth">13.09.2020</time>
+            </div>
+          </div>
+          <button class="banned__button button-second">Unban</button>
+        </div>
+
+        <div class="banned__comment">
+          <div class="comment">
+            <div class="comment__image"></div>
+            <div class="comment__body">
+                <p class="comment__text text-sixth">
+                  <span class="comment__name">Eden Pepe</span>
+                  <span class="comment__mention">Za lupa</span>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr
+                </p>
+                <time class="comment__date text-sixth">13.09.2020</time>
+            </div>
+          </div>
+          <button class="banned__button button-second">Unban</button>
+        </div>
+
+
+      </div>
+    </section>
+
     <favorites/>
   </main>
 </template>
