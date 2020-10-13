b94ce9ff21e921a5e59aec90b32b153f4d5a6368
x4e554c4c <wanderingdwarf03@gmail.com>
2020-10-13 19:30:10 +0300

    Changed review component (Vue)

--- app/src/pages/profile/ReviewsEditor.vue
+++ app/src/pages/profile/ReviewsEditor.vue
@@ -2,59 +2,68 @@
   <section class="reviews-management container">
     <h3 class="heading-third">Reviews manage</h3>
     <request-form
-      class="reviews-management__body"
       ref="form"
       :submit-callback="submit">
       
-      <div
-        v-for="(review, key) in reviews"
-        class="reviews__card"
-        :key="key">
+      <transition-group 
+        class="reviews-management__body"
+        name="list"
+        @before-leave="setAbsolute">
 
-        <div class="reviews__card-person">
-          <div class="reviews__card-photo"></div>
-          <h5 class="reviews__card-name heading-fifth">Person Name</h5>
-        </div>
-        <div class="reviews__card-content">
-          <h5 class="reviews__card-title heading-fifth">{{ review.title }}</h5>
-          <p class="reviews__card-text text-third">
-            {{ review.text }}
-          </p>
-        </div>
-        <div class="reviews__card-grade">
-          <div class="reviews__card-stars">
-            <div 
-              v-for="id in 5"
-              class="reviews__card-star"
-              :key="id"
-              :class="{
-                'reviews__card-star--full': id <= review.grade 
-              }">
-            </div>
+        <div
+          v-for="(review, key) in reviews"
+          class="reviews__card"
+          :key="key">
 
+          <div class="reviews__card-person">
+            <div class="reviews__card-photo"></div>
+            <h5 class="reviews__card-name heading-fifth">Person Name</h5>
           </div>
-          <span class="reviews__card-rating">5.0</span>
-        </div>
-        <div class="reviews__card-button-group">
-          <submit-button
-            class="reviews__card-button reviews__card-button--delete button-second"
-            type="button"
-            :loading="review.declining"
-            @click.native="onDeclineClick(review)">
-            
-            delete
-          </submit-button>
+          <div class="reviews__card-content">
+            <h5 class="reviews__card-title heading-fifth">
+              {{ review.title }}
+            </h5>
+            <p class="reviews__card-text text-third">
+              {{ review.text }}
+            </p>
+          </div>
+          <div class="reviews__card-grade">
+            <div class="reviews__card-stars">
+              <div 
+                v-for="id in 5"
+                class="reviews__card-star"
+                :key="id"
+                :class="{
+                  'reviews__card-star--full': id <= review.grade 
+                }">
+              </div>
+            </div>
+            <span class="reviews__card-rating">5.0</span>
+          </div>
+          <div class="reviews__card-button-group">
+            <submit-button
+              class="reviews__card-button reviews__card-button--delete button-second"
+              type="button"
+              :loading="review.declining"
+              :disabled="loading"
+              @click.native="onDeclineClick(review)">
+              
+              delete
+            </submit-button>
 
-          <submit-button
-            class="reviews__card-button reviews__card-button--apply button-second"
-            type="button"
-            :loading="review.verifying"
-            @click.native="onVerifyClick(review)">
-            
-            apply
-          </submit-button>
+            <submit-button
+              class="reviews__card-button reviews__card-button--apply button-second"
+              type="button"
+              :loading="review.verifying"
+              :disabled="loading"
+              @click.native="onVerifyClick(review)">
+              
+              apply
+            </submit-button>
+          </div>
         </div>
-      </div>
+      </transition-group>
+   
     </request-form>
   </section>
 </template>
@@ -78,7 +87,7 @@ export default {
   },
 
   computed: {
-    sending: {
+    loading: {
       cache: false,
 
       get() {
@@ -89,15 +98,28 @@ export default {
   
   beforeMount() {
     this.load();
+
+    window.add = () => 
+    {
+      this.reviews.unshift({});
+    }
   },
 
   methods: {
+    setAbsolute(review) {
+			Object.assign(review.style, {
+				position: 'absolute',
+				width: review.offsetWidth + 'px',
+				top: review.offsetTop + 'px',
+				left: review.offsetLeft + 'px'
+			})
+		},
     onDeclineClick(review) {
       // defining callback for submit function 
       this.$options.submitCallback = () =>
         this.decline(review);
 
-      // mannualy send request 
+      // mannualy sending request 
       let form = this.$refs.form;
 
       form.send();
@@ -108,19 +130,30 @@ export default {
       this.$options.submitCallback = () =>
         this.verify(review);
 
-      // mannualy send request 
+      // mannualy sending request 
       let form = this.$refs.form;
 
       form.send();
     },
 
+    removeFromList(review) {
+      for (let i in this.reviews)
+      {
+        let item = this.reviews[i];
+
+        if (item.id !== review.id)
+          continue;
+        
+        this.reviews.splice(i, 1);  
+      }
+    },
+
     async decline(review) {
       review.declining = true;
-      // await Reviews.delete(review.id);
-
-      await new Promise((resolve) => setTimeout(resolve, 4000));
-
+      await Reviews.delete(review.id);
       review.declining = false;
+
+      this.removeFromList(review);
     },
 
     async verify(review) {
@@ -150,7 +183,34 @@ export default {
         this.reviews.push(review);
       }
     }
-
   }
 };
 </script>
+
+<style scoped>
+.reviews-management__body {
+  position: relative;
+}
+
+.list-enter {
+  opacity: 0;
+}
+
+.list-enter-active {
+  opacity: 1;
+}
+
+.list-move {
+  transition: transform 1s ease-in-out,
+	  					opacity 1s ease-in-out;
+}
+
+.list-leave {
+  opacity: 1;
+  transition: opacity 1s;
+}
+
+.list-leave-active {
+  opacity: 0;
+}
+</style>
