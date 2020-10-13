23ff467909a5efa95fa0d73a5e5b1167cfe37d65
x4e554c4c <wanderingdwarf03@gmail.com>
2020-10-13 21:31:34 +0300

    Minor fixes (Vue)

--- app/src/pages/profile/ReviewsEditor.vue
+++ app/src/pages/profile/ReviewsEditor.vue
@@ -15,53 +15,54 @@
           v-for="review in reviews"
           :key="review.id">
           
-            <div class="reviews__card-person">
-              <div class="reviews__card-photo"></div>
-              <h5 class="reviews__card-name heading-fifth">Person Name</h5>
-            </div>
-            <div class="reviews__card-content">
-              <h5 class="reviews__card-title heading-fifth">
-                {{ review.title }}
-              </h5>
-              <p class="reviews__card-text text-third">
-                {{ review.text }}
-              </p>
-            </div>
-            <div class="reviews__card-grade">
-              <div class="reviews__card-stars">
-                <div 
-                  v-for="id in 5"
-                  class="reviews__card-star"
-                  :key="id"
-                  :class="{
-                    'reviews__card-star--full': id <= review.grade 
-                  }">
-                </div>
+          <div class="reviews__card-person">
+            <div class="reviews__card-photo"></div>
+            <h5 class="reviews__card-name heading-fifth">Person Name</h5>
+          </div>
+          
+          <div class="reviews__card-content">
+            <h5 class="reviews__card-title heading-fifth">
+              {{ review.title }}
+            </h5>
+            <p class="reviews__card-text text-third">
+              {{ review.text }}
+            </p>
+          </div>
+          <div class="reviews__card-grade">
+            <div class="reviews__card-stars">
+              <div 
+                v-for="id in 5"
+                class="reviews__card-star"
+                :key="id"
+                :class="{
+                  'reviews__card-star--full': id <= review.grade 
+                }">
               </div>
-              <span class="reviews__card-rating">5.0</span>
             </div>
-            <div class="reviews__card-button-group">
-              <submit-button
-                class="reviews__card-button reviews__card-button--delete button-second"
-                type="button"
-                :loading="review.declining"
-                :disabled="loading"
-                @click.native="onDeclineClick(review)">
-                
-                delete
-              </submit-button>
+            <span class="reviews__card-rating">5.0</span>
+          </div>
+          <div class="reviews__card-button-group">
+            <submit-button
+              class="reviews__card-button reviews__card-button--delete button-second"
+              type="button"
+              :loading="review.declining"
+              :disabled="loading"
+              @click.native="onDeclineClick(review)">
+              
+              delete
+            </submit-button>
 
-              <submit-button
-                class="reviews__card-button reviews__card-button--apply button-second"
-                type="button"
-                :loading="review.verifying"
-                :disabled="loading"
-                @click.native="onVerifyClick(review)">
-                
-                apply
-              </submit-button>
-            </div>
-          </swiper-slide>
+            <submit-button
+              class="reviews__card-button reviews__card-button--apply button-second"
+              type="button"
+              :loading="review.verifying"
+              :disabled="loading"
+              @click.native="onVerifyClick(review)">
+              
+              apply
+            </submit-button>
+          </div>
+        </swiper-slide>
       </swiper>
     </request-form>
   </section>
