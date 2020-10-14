b9b48c01b3250e48cff66bbfb27c2cb41d21a13e
x4e554c4c <wanderingdwarf03@gmail.com>
2020-10-14 14:24:53 +0300

    Added leave enter transtion to reviews editor (Vue)

--- app/src/pages/home/ReviewsEditor.vue
+++ app/src/pages/home/ReviewsEditor.vue
@@ -1,48 +1,50 @@
 <template>
-  <section
-    v-if="shown" 
-    class="modal container">
+  <transition name="fade">
+    <section
+      v-if="shown" 
+      class="modal container">
 
-    <request-form 
-      class="leave-review"
-      ref="form"
-      :submit-callback="submit">
+      <request-form 
+        class="leave-review"
+        ref="form"
+        :submit-callback="submit">
 
-      <div class="leave-review__header">
-        <h5 class="heading-fifth">Leave your review</h5>
-        <button 
-          class="leave-review__close-button"
-          type="button"
-          @click="hide">
-        </button>
-      </div>
+        <div class="leave-review__header">
+          <h5 class="heading-fifth">Leave your review</h5>
+          <button 
+            class="leave-review__close-button"
+            type="button"
+            @click="hide">
+          </button>
+        </div>
 
-      <div class="leave-review__body">
-        <label class="leave-review__label text-fourth" for="">Title of review</label>
-        <input 
-          class="leave-review__input input-second"
-          placeholder="your title"
-          name="title"
-          type="text"/>
+        <div class="leave-review__body">
+          <label class="leave-review__label text-fourth" for="">Title of review</label>
+          <input 
+            class="leave-review__input input-second"
+            placeholder="your title"
+            name="title"
+            type="text"/>
 
-        <label class="leave-review__label text-fourth" for="">Text of your review</label>
-        <textarea 
-          class="leave-review__textarea textarea-second" 
-          placeholder="your review"
-          name="text"/>
-      
-        <label class="leave-review__label text-fourth" for="">Grade</label>
-        <rate-stars name="grade"/>
-      </div>
-      <div class="leave-review__footer">
-        <submit-button 
-          class="leave-review__button button-second"
-          :loading="isLoading()">
-          Confirm
-        </submit-button> 
-      </div>
-    </request-form>
-  </section>
+          <label class="leave-review__label text-fourth" for="">Text of your review</label>
+          <textarea 
+            class="leave-review__textarea textarea-second" 
+            placeholder="your review"
+            name="text"/>
+        
+          <label class="leave-review__label text-fourth" for="">Grade</label>
+          <rate-stars name="grade"/>
+        </div>
+        <div class="leave-review__footer">
+          <submit-button 
+            class="leave-review__button button-second"
+            :loading="isLoading()">
+            Confirm
+          </submit-button> 
+        </div>
+      </request-form>
+    </section>
+  </transition>
 </template>
 
 <script>
@@ -94,3 +96,24 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+fade-leave {
+  opacity: 1;
+  transition: opacity .5s;
+}
+
+fade-enter {
+  opacity: 0;  
+  transition: opacity .5s;
+}
+
+fade-leave-active {
+  opacity: 0;
+}
+
+fade-enter-active {
+  opacity: 1;
+}
+</style>
