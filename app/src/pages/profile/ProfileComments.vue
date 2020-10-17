0a8001405412ccce23094fa831a35a51ceb4efe5
x4e554c4c <wanderingdwarf03@gmail.com>
2020-10-17 19:48:43 +0300

    Aded comments empty overlay (Vue)

--- app/src/pages/profile/ProfileComments.vue
+++ app/src/pages/profile/ProfileComments.vue
@@ -1,39 +1,48 @@
 <template>
-    <div class="management__tab-body">
-  <!-- <div class="management__account-overlay">
-    <object class="management__account-overlay-image" data="/img/svg/comments-overlay.svg" type="image/svg+xml"></object>
-  </div> -->
-  <button class="management__comments-delete-button text-fourth"><span class="material-icons-round">delete_forever</span>delete all</button>
-  <div class="management__tab-content">
-    <div class="management__tab-scrollable">
-      <div
-        class="management__comment"
-        v-for="comment in comments"
-        :key="comment.id">
+  <div class="management__tab-body">
+    <transition name="fade">
+      <div 
+        class="management__account-overlay"
+        v-if="empty">
 
-        <div class="management__comment-image"></div>
-        <div class="managment__comment-wrapper">
-          <div class="managment__comment-header">
-            <h6 class="heading-sixth">{{ comment.post.title }}</h6>
-            <button class="management__comment-delete-button">
-              <span class="material-icons-round">delete_forever</span>
-            </button>
-          </div>
-          <div class="comment">
-            <div class="comment__body">
-              <p class="comment__text text-sixth">
-                <span class="comment__name">{{ comment.user.name }}</span>
-                <!-- <span class="comment__mention">{{}}</span> -->
-                {{ comment.message }}
-              </p>
-              <time class="comment__date text-sixth">{{ comment.createdAt }}</time>
+        <object class="management__account-overlay-image" data="/img/svg/comments-overlay.svg" type="image/svg+xml"></object>
+      </div>
+    </transition>
+
+    <button class="management__comments-delete-button text-fourth">
+      <span class="material-icons-round">delete_forever</span>delete all
+    </button>
+    
+    <div class="management__tab-content">
+      <div class="management__tab-scrollable">
+        <div
+          class="management__comment"
+          v-for="comment in comments"
+          :key="comment.id">
+
+          <div class="management__comment-image"></div>
+          <div class="managment__comment-wrapper">
+            <div class="managment__comment-header">
+              <h6 class="heading-sixth">{{ comment.post.title }}</h6>
+              <button class="management__comment-delete-button">
+                <span class="material-icons-round">delete_forever</span>
+              </button>
+            </div>
+            <div class="comment">
+              <div class="comment__body">
+                <p class="comment__text text-sixth">
+                  <span class="comment__name">{{ comment.user.name }}</span>
+                  <!-- <span class="comment__mention">{{}}</span> -->
+                  {{ comment.message }}
+                </p>
+                <time class="comment__date text-sixth">{{ comment.createdAt }}</time>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -51,6 +60,10 @@ export default {
   computed: {
     loading() {
       return this.comments === null;
+    },
+
+    empty() {
+      return !!!this.loading && this.comments.length === 0
     }
   },
 
@@ -65,3 +78,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+	.fade-enter-active, .fade-leave-active {
+	  transition: opacity 2s;
+	}
+	.fade-enter, .fade-leave-to {
+	  opacity: 0;
+	}
+</style>
