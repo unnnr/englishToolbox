b4e8a1a914732796f0f771b4a43582b39a257110
x4e554c4c <wanderingdwarf03@gmail.com>
2020-10-25 22:56:10 +0200

    Added about me 'data'  (Vue)

--- app/src/pages/home/AboutMe.vue
+++ app/src/pages/home/AboutMe.vue
@@ -14,47 +14,19 @@
       </p>
     </div>
 
-<!--     <div 
+    <div 
       class="about-me__merit"
-      v-for="({descrition, title}, index) in achivments"
+      v-for="({description, title}, index) in achivments"
       :key="index">
 
       <div class="about-me__merit-picture about-me__picture--first">
-        <h4 class="about-me__merit-title heading-fourth">{{ title }}</h4>
+        <h4 class="about-me__merit-title heading-fourth">
+          {{ title }}
+        </h4>
       </div>
-      <p class="about-me__merit-text text-second">{{ descrition }}</p>
-    </div> -->
-
-    <div class="about-me__merit">
-      <div class="about-me__merit-picture about-me__picture--first">
-        <h4 class="about-me__merit-title heading-fourth">Nulla porttitor</h4>
-      </div>
+      
       <p class="about-me__merit-text text-second">
-        Elit eget gravida cum sociis natoque penatibus. Lobortis mattis
-        aliquam faucibus purus in massa tempor nec.
-        Rhoncus est pellentesque elit ullam.
-      </p>
-    </div>
-
-    <div class="about-me__merit">
-      <div class="about-me__merit-picture about-me__picture--first">
-        <h4 class="about-me__merit-title heading-fourth">Nulla porttitor</h4>
-      </div>
-      <p class="about-me__merit-text text-second">
-        Elit eget gravida cum sociis natoque penatibus. Lobortis mattis
-        aliquam faucibus purus in massa tempor nec.
-        Rhoncus est pellentesque elit ullam.
-      </p>
-    </div>
-
-    <div class="about-me__merit">
-      <div class="about-me__merit-picture about-me__picture--first">
-        <h4 class="about-me__merit-title heading-fourth">Nulla porttitor</h4>
-      </div>
-      <p class="about-me__merit-text text-second">
-        Elit eget gravida cum sociis natoque penatibus. Lobortis mattis
-        aliquam faucibus purus in massa tempor nec.
-        Rhoncus est pellentesque elit ullam.
+        {{ description }}
       </p>
     </div>
 
@@ -67,27 +39,27 @@ export default {
     return {
       image: '',
 
-      description: 'My name is Mary Cousins, lorem ipsum dolor sit amet, consectetur adipiscing'
-                 + 'elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Posuere morbi leo urna molestie at'
-                 + 'Morbi tristique senectus et netus et malesuada fames ac turpis. Etiam sit amet nisl purus in mollis nunc sed.',
+      description: 'My name is Mary Cousins, lorem ipsum dolor sit amet, consectetur adipiscing '
+                 + 'elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Posuere morbi leo urna molestie at '
+                 + 'Morbi tristique senectus et netus et malesuada fames ac turpis. Etiam sit amet nisl purus in mollis nunc sed. ',
     
       achivments: [
         {
           title: 'Nulla porttitor',
-          description: 'Elit eget gravida cum sociis natoque penatibus. Lobortis mattis'
-                     + 'aliquam faucibus purus in massa tempor nec.'
+          description: 'Elit eget gravida cum sociis natoque penatibus. Lobortis mattis '
+                     + 'aliquam faucibus purus in massa tempor nec. '
+                     + 'Rhoncus est pellentesque elit ullam. ' 
+        },
+        {
+          title: 'Nulla porttitor',
+          description: 'Elit eget gravida cum sociis natoque penatibus. Lobortis mattis '
+                     + 'aliquam faucibus purus in massa tempor nec. '
                      + 'Rhoncus est pellentesque elit ullam.'
         },
         {
           title: 'Nulla porttitor',
-          description: 'Elit eget gravida cum sociis natoque penatibus. Lobortis mattis'
-                     + 'aliquam faucibus purus in massa tempor nec.'
-                     + 'Rhoncus est pellentesque elit ullam.'
-        },
-        {
-          title: 'Nulla porttitor',
-          description: 'Elit eget gravida cum sociis natoque penatibus. Lobortis mattis'
-                     + 'aliquam faucibus purus in massa tempor nec.'
+          description: 'Elit eget gravida cum sociis natoque penatibus. Lobortis mattis '
+                     + 'aliquam faucibus purus in massa tempor nec. '
                      + 'Rhoncus est pellentesque elit ullam.'
         }
       ],
