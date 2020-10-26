9c9679e59e26966aed6cc7567755d15c7b44be9c
x4e554c4c <wanderingdwarf03@gmail.com>
2020-10-27 01:14:38 +0200

    Added new post addition layout (Vue)

--- app/src/components/posts_new/PostAddition.vue
+++ app/src/components/posts_new/PostAddition.vue
@@ -9,76 +9,26 @@
 
     <div class="addition__tabs">
 
-      <div class="addition__tab description">
-        <div class="addition__tab-header">
-          <h5>Description title</h5>
-        </div>
-        <div class="addition__tab-body">
-          <p class="description__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Vel turpis nunc eget lorem dolor sed viverra ipsum. Vestibulum mattis ullamcorper
-            velit sed ullamcorper morbi tincidunt ornare massa. Id venenatis a condimentum vitae sapien. Tincidunt
-            tortor aliquam nulla facilisi cras. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor
-            aliquam nulla. Dolor morbi non arcu risus quis varius quam quisque. Cras semper auctor neque vitae. Enim
-            ut tellus elementum sagittis vitae et leo duis. Adipiscing vitae proin sagittis nisl rhoncus. Nec
-            ultrices dui sapien eget. Vitae ultricies leo integer malesuada.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Vel turpis nunc eget lorem dolor sed viverra ipsum. Vestibulum mattis ullamcorper
-            velit sed ullamcorper morbi tincidunt ornare massa. Id venenatis a condimentum vitae sapien. Tincidunt
-            tortor aliquam nulla facilisi cras. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor
-            aliquam nulla. Dolor morbi non arcu risus quis varius quam quisque. Cras semper auctor neque vitae. Enim
-            ut tellus elementum sagittis vitae et leo duis. Adipiscing vitae proin sagittis nisl rhoncus. Nec
-            ultrices dui sapien eget. Vitae ultricies leo integer malesuada.
-          </p>
-        </div>
-        <div class="addition__tab-footer">
-          <time class="description__date">Oct 7 2020</time>
-          <span class="description__views">123</span>
-        </div>
-      </div>
+      <post-info/>
 
-      <div class="addition__tab comments">
-        <div class="addition__tab-header">
-          <h5>Comments</h5>
-        </div>
-        <div class="addition__tab-body">
-          <small class="comments__count">6 comments</small>
-          <div class="comment"></div>
-          <div class="comment"></div>
-          <div class="comment"></div>
-          <div class="comment"></div>
-          <div class="comment"></div>
-          <div class="comment"></div>
-          <div class="comment"></div>
-          <div class="comment"></div>
-        </div>
-        <div class="addition__tab-footer">
-          <div class="area"></div>
-        </div>
-      </div>
+      <post-comments/>
 
-      <div class="addition__tab editor">
-        <div class="addition__tab-header">
-          <h5>Video editing</h5>
-        </div>
-        <div class="addition__tab-body">
-          <p class="description__text"></p>
-        </div>
-        <div class="addition__tab-footer">
-          <button>Confirm</button>
-        </div>
-      </div>
-
+      <post-editor/>
     </div>
-
-
   </div>
 </template>
 
 <script>
 
+import PostComments from '@components/posts_new/PostComments'
+import PostEditor from '@components/posts_new/PostEditor'
+import PostInfo from '@components/posts_new/PostInfo'
 
 export default {
-  
+  components: {
+    PostComments,
+    PostEditor,
+    PostInfo
+  }
 }
 </script>
---
+++ app/src/components/posts_new/PostInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="addition__tab description">
+    <div class="addition__tab-header">
+      <h5>Description title</h5>
+    </div>
+    <div class="addition__tab-body">
+      <p class="description__text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Vel turpis nunc eget lorem dolor sed viverra ipsum. Vestibulum mattis ullamcorper
+        velit sed ullamcorper morbi tincidunt ornare massa. Id venenatis a condimentum vitae sapien. Tincidunt
+        tortor aliquam nulla facilisi cras. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor
+        aliquam nulla. Dolor morbi non arcu risus quis varius quam quisque. Cras semper auctor neque vitae. Enim
+        ut tellus elementum sagittis vitae et leo duis. Adipiscing vitae proin sagittis nisl rhoncus. Nec
+        ultrices dui sapien eget. Vitae ultricies leo integer malesuada.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Vel turpis nunc eget lorem dolor sed viverra ipsum. Vestibulum mattis ullamcorper
+        velit sed ullamcorper morbi tincidunt ornare massa. Id venenatis a condimentum vitae sapien. Tincidunt
+        tortor aliquam nulla facilisi cras. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor
+        aliquam nulla. Dolor morbi non arcu risus quis varius quam quisque. Cras semper auctor neque vitae. Enim
+        ut tellus elementum sagittis vitae et leo duis. Adipiscing vitae proin sagittis nisl rhoncus. Nec
+        ultrices dui sapien eget. Vitae ultricies leo integer malesuada.
+      </p>
+    </div>
+    <div class="addition__tab-footer">
+      <time class="description__date">Oct 7 2020</time>
+      <span class="description__views">123</span>
+    </div>
+  </div>
+</template>
