972b5a51be644bdf25e58da2ad18e56cc5e75c12
x4e554c4c <wanderingdwarf03@gmail.com>
2020-10-27 03:14:06 +0200

    Added post comments loading (vue)

--- app/src/components/posts_new/PostComments.vue
+++ app/src/components/posts_new/PostComments.vue
@@ -4,18 +4,47 @@
       <h5>Comments</h5>
     </div>
     <div class="addition__tab-body">
-      <small class="comments__count">6 comments</small>
-      <div class="comment"></div>
-      <div class="comment"></div>
-      <div class="comment"></div>
-      <div class="comment"></div>
-      <div class="comment"></div>
-      <div class="comment"></div>
-      <div class="comment"></div>
-      <div class="comment"></div>
+      <small class="comments__count">{{ counter }}</small>
+
+      <div 
+        class="comment"
+        v-for="{id, message, createdAt, user} in comments"
+        :key="id">
+
+        {{ user }} {{createdAt}} {{ message }}
+      
+      </div>
     </div>
     <div class="addition__tab-footer">
       <div class="area"></div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  inject: [ '$target' ],
+
+  computed: {
+    target() {
+      return this.$target();
+    },
+
+    comments() {
+      return this.target ? this.target.comments : [
+        { message: 'some' },
+        { message: 'some' },
+        { message: 'some' },
+        { message: 'some' },
+        { message: 'some' },
+        { message: 'some' },
+        { message: 'some' },
+      ];
+    },
+
+    counter() {
+      return this.comments.length + ' comments';
+    }
+  }
+}
+</script>
