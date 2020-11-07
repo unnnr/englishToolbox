7d3c01606169e37a5dd2682757f1c670fa8ef8b9
x4e554c4c <wanderingdwarf03@gmail.com>
2020-11-07 21:25:29 +0200

    Updated fakeData (Vue)

--- app/src/services/FakeData.js
+++ app/src/services/FakeData.js
@@ -2,142 +2,161 @@ import Faker from 'faker/locale/ja'
 import FormatedDate from '@services/FormatedDate'
 
 class FakeData {
-    generateImage() 
+  generateImage() 
+  {
+    return Faker.image.image();
+  };
+  
+  generateTitle() 
+  {
+    return Faker.lorem.sentence();
+  }
+  
+  generateDescription() 
+  {
+    return Faker.lorem.paragraph();
+  }
+  
+  generateViews() 
+  {
+    return Faker.random.number(3333);
+  }
+  
+  generateDate() 
+  {
+    return FormatedDate.parse(Faker.date.past());
+  }
+  
+  generateColor() 
+  {
+    return '#' + Math.floor(Math.random() * Math.pow(16, 6)).toString(16).padStart(6, '0');
+  }
+  
+  generateTags() 
+  {
+    function shuffle(array) {
+      for (let i in array)
+      {
+        let randomIndex = Math.floor(Math.random() * array.length);
+        let randomItem = array[randomIndex];
+
+        let currentItem = array[i];
+        array[randomIndex] = currentItem;
+        array[i] = randomItem;
+      }
+      
+      return array;
+    }
+
+    let tags = [{"id":1,"label":"pink","color":"pink","default":null},{"id":2,"label":"paleturquoise","color":"paleturquoise","default":null},{"id":3,"label":"powderblue","color":"powderblue","default":null},{"id":4,"label":"plum","color":"plum","default":null},{"id":5,"label":"bisque","color":"bisque","default":null},{"id":6,"label":"lightgreen","color":"lightgreen","default":null},{"id":7,"label":"lightblue","color":"lightblue","default":null},{"id":8,"label":"video","color":"lightblue","default":1},{"id":9,"label":"audio","color":"plum","default":1}];
+    let tagsCount = Math.floor(Math.random() * 5);
+    let shuffled = shuffle(tags).splice(0, tagsCount);
+
+   /*  for (let i = 0; i < tagsCount; i++)
     {
-        return Faker.image.image();
+      tags.push({
+        color: this.generateColor(),
+        label: Faker.lorem.word()
+      })
+
+      
+    } */
+
+    return shuffled;
+  }
+  
+  generateMainTag() 
+  {
+    return {
+      color: this.generateColor(),
+      label: Faker.lorem.word(),
+    }
+  }
+
+  createComments()
+  {
+    let comments = [];
+    let count = Faker.random.number(15);
+
+    for (let i = 0; i < 20 + count; i++)
+    {
+      comments.push({ 
+        message: Faker.lorem.sentence(),
+
+        createdAt: FormatedDate.parse(Faker.date.past()),
+
+        user: {
+          name: Faker.name.firstName(), 
+          avatar: Faker.image.avatar()
+        }
+      })
+    }
+  
+    return comments;
+  }
+  
+  generateVideoId() 
+  {
+    let videos = [ 
+      'eZe4Q_58UTU','BOa0zQBRs_M','Et7O5-CzJZg',
+      'ynLpZGegiJE','JB0A8Me8EKk','Vg1mpD1BICI',
+      'uMnGzVPUEB4','ww-LFK1-GRg','3raVUTPAd-w',
+      '2uQ58Xwx1V4','2f8Q70JZM9w','Mckcmh-OU5M',
+      'YF3pj_3mdMc','gaGrHUekGrc','dcEiFOLXy0c',
+      'fh3EdeGNKus','QpepXSfYEBk','yKvu63qXSp8',
+      'adQPQwXG6AM','KLLVXw335u4'];
+
+    let index = Math.floor(Math.random() * videos.length);
+    
+    return videos[index]; 
+  }
+  
+  createPost(videoId) 
+  {
+    let youtubeID;
+
+    if (videoId)
+      youtubeID = videoId;
+    else
+      youtubeID = this.generateVideoId();
+
+    let post  = {
+      id: Faker.random.number(1000),
+
+      title: this.generateTitle(),
+
+      description: this.generateDescription(),
+
+      views: this.generateViews(),
+      
+      createdAt: this.generateDate(),
+
+      tags: this.generateTags(),
+
+      mainTag: this.generateMainTag(),
+
+      comments: this.createComments(),
+
+      videoId: youtubeID
     };
-    
-    generateTitle() 
-    {
-        return Faker.lorem.sentence();
-    }
-    
-    generateDescription() 
-    {
-        return Faker.lorem.paragraph();
-    }
-    
-    generateViews() 
-    {
-        return Faker.random.number(3333);
-    }
-    
-    generateDate() 
-    {
-        return FormatedDate.parse(Faker.date.past());
-    }
-    
-    generateColor() 
-    {
-        return '#' + Math.floor(Math.random() * Math.pow(16, 6)).toString(16).padStart(6, '0');
-    }
-    
-    generateTags() 
-    {
-        let tags = [];
-        let tagsCount = Math.floor(Math.random() * 5);
-        
-        for (let i = 0; i < tagsCount; i++)
-        {
-          tags.push({
-            color: this.generateColor(),
-            label: Faker.lorem.word()
-          })
-        }
-    
-        return tags;
-    }
-    
-    generateMainTag() 
-    {
-        return {
-          color: this.generateColor(),
-          label: Faker.lorem.word(),
-        }
-    }
 
-    createComments()
-    {
-        let comments = [];
-        let count = Faker.random.number(15);
-    
-        for (let i = 0; i < 20 + count; i++)
-        {
-          comments.push({ 
-            message: Faker.lorem.sentence(),
-    
-            createdAt: FormatedDate.parse(Faker.date.past()),
-    
-            user: {
-              name: Faker.name.firstName(), 
-              avatar: Faker.image.avatar()
-            }
-          })
-        }
-    
-        return comments;
-    }
-    
-    generateVideoId() 
-    {
-        let videos = [ 
-          'eZe4Q_58UTU','BOa0zQBRs_M','Et7O5-CzJZg',
-          'ynLpZGegiJE','JB0A8Me8EKk','Vg1mpD1BICI',
-          'uMnGzVPUEB4','ww-LFK1-GRg','3raVUTPAd-w',
-          '2uQ58Xwx1V4','2f8Q70JZM9w','Mckcmh-OU5M',
-          'YF3pj_3mdMc','gaGrHUekGrc','dcEiFOLXy0c',
-          'fh3EdeGNKus','QpepXSfYEBk','yKvu63qXSp8',
-          'adQPQwXG6AM','KLLVXw335u4'];
-    
-        let index = Math.floor(Math.random() * videos.length);
-        
-        return videos[index]; 
-    }
-    
-    createPost(videoId) 
-    {
-        let youtubeID;
+    post.thumbnail = `https://img.youtube.com/vi/${post.videoId}/sddefault.jpg`;
 
-        if (videoId)
-            youtubeID = videoId;
-        else
-            youtubeID = this.generateVideoId();
+    return post;
+  }
 
-        let post  = {
-          id: Faker.random.number(1000),
-    
-          title: this.generateTitle(),
-    
-          description: this.generateDescription(),
-    
-          views: this.generateViews(),
-          
-          createdAt: this.generateDate(),
-    
-          tags: this.generateTags(),
-    
-          mainTag: this.generateMainTag(),
-    
-          comments: this.createComments(),
-    
-          videoId: youtubeID
-        };
-    
-        post.thumbnail = `https://img.youtube.com/vi/${post.videoId}/sddefault.jpg`;
-    
-        return post;
-    }
+  generatePosts() 
+  {
+    let posts = [];
+    let count =  1 + Faker.random.number(10);
 
-    generatePosts() 
-    {
-        let posts = [];
-        let count =  1 + Faker.random.number(10);
-    
-        for (let i = 0; i < count; i++)
-          posts.push(this.createPost());
-    
-        return posts;
-    }
+    for (let i = 0; i < count; i++)
+      posts.push(this.createPost());
+
+    return posts;
+  }
 }
 
-export default new FakeData();
+window.FakeData = new FakeData();
+
+export default window.FakeData; 
