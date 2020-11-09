9075f4c789c1a73773a78ed0c93c443120a83a1a
x4e554c4c <wanderingdwarf03@gmail.com>
2020-11-09 18:08:09 +0200

    Addded audio page (Vue)

---
+++ app/src/pages/Audio.vue
@@ -0,0 +1,61 @@
+<template>
+ <main class="posts">
+
+    <filter-bar/>
+
+    <post-selected
+      :editor-has-changes="editorHasChanges"
+      :creator-has-changes="creatorHasChanges">
+
+      <template #creator>
+      </template>
+
+      <template #editor>
+      </template>
+
+      <template #player>
+        <audio-player/>
+      </template>
+
+    </post-selected>
+    
+    <pool ref="pool" :posts="posts"/>
+
+  </main>
+</template>
+
+<script>
+
+import PostSelected from '@components/posts_new/PostSelected'
+import FilterBar from '@components/layout/FilterBar'
+import Pool from '@components/cards_new/Pool'
+import HandlePost from'@mixins/HandlePost'
+import AudioPlayer from '@pages/audio/AudioPlayer'
+
+export default {
+  components: {
+    PostSelected,
+    AudioPlayer,
+    FilterBar,
+    Pool
+  },
+
+  mixins: [ HandlePost ],
+
+  data() {
+    return {
+      posts: []
+    }
+  },
+
+  mounted() {
+    this.loadPosts();
+  },
+
+  methods: {
+    loadPosts() {
+      this.posts = [ {}, {} ]
+    }
+  }
+}
+</script>
