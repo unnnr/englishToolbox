dbb26e563ef18b2ffab56121b46d2d95eff4be2c
x4e554c4c <wanderingdwarf03@gmail.com>
2020-11-10 21:01:09 +0200

    Minor fixes (Vue)

--- app/src/components/tags/ShortenTagList.vue
+++ app/src/components/tags/ShortenTagList.vue
@@ -1,24 +1,5 @@
 <template>
    <div class="card__tags">
- <!--    <div 
-      class="card__tag tag tag--circle"
-
-      v-for="({color}, index) in tags"
-      :key="index"
-      
-      :style="{'background-color': color}">
-    </div> -->
-
-<!--     <button 
-      class="card__tag-main tag tag--main"
-      
-      v-if="mainTagShown"
-      :style="{'background-color': mainTag.color}">>
-      
-      {{ mainTag.label }} 
-    </button>
- -->
-
     <tag
       class="card__tag"
 
@@ -27,8 +8,7 @@
       
       v-tooltip="label"
       :color="color"
-      circled
-      />
+      circled/>
 
     <tag
       v-if="mainTagShown"
