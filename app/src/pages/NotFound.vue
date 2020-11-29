042565e3de33befd880aa37f296009552a5ffde7
Crybaka <kryptolone@protonmail.com>
2020-11-29 17:18:32 +0200

    Page 404

--- app/src/pages/NotFound.vue
+++ app/src/pages/NotFound.vue
@@ -1,5 +1,8 @@
 <template>
-  <main>
-    <h1>404</h1>
+  <main class="not-found container">
+    <div class="not-found__body">
+        <img src="/img/svg/404.svg" alt="">
+        <button class="button-secondary">Go back!</button>
+    </div>
   </main>
 </template>
