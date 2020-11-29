cf58c79db89dc6b5dd6a7cbb8d3014a1d734b5b0
Crybaka <kryptolone@protonmail.com>
2020-11-30 00:13:09 +0200

    Page 404 minor fix

--- app/src/pages/NotFound.vue
+++ app/src/pages/NotFound.vue
@@ -1,8 +1,6 @@
 <template>
   <main class="not-found container">
-    <div class="not-found__body">
-        <img src="/img/svg/404.svg" alt="">
-        <button class="button-secondary">Go back!</button>
-    </div>
+    <img src="/img/svg/404.svg" alt="">
+    <button class="button-secondary">Go back!</button>
   </main>
 </template>
