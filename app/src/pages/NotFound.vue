99766704e21e0e0f81790a2ccc883a3cde85debc
x4e554c4c <wanderingdwarf03@gmail.com>
2020-12-09 21:36:20 +0200

    go back (Vue)

--- app/src/pages/NotFound.vue
+++ app/src/pages/NotFound.vue
@@ -1,6 +1,22 @@
 <template>
   <main class="not-found container">
     <img src="/img/svg/404.svg" alt="">
-    <button class="button-secondary">Go back!</button>
+    <button 
+      class="button-secondary"
+      @click="back">
+      
+      Go back!
+    </button>
   </main>
 </template>
+
+<script>
+export default {
+
+  methods: {
+    back() {
+      this.$router.go(-1);
+    }
+  }
+}
+</script>
