bc35939b45260b529311eb06410ac99dc985e919
x4e554c4c <wanderingdwarf03@gmail.com>
2020-12-29 05:26:38 +0200

    Fixes (Matcher)

--- app/src/services/matcher/Matcher.js
+++ app/src/services/matcher/Matcher.js
@@ -24,39 +24,43 @@ export default class Matcher {
         height = Config.world.height,
         size = 100;
     
-    var top = Bodies.rectangle( width / 2, size / -2, width, size, { isStatic: true }),
+    let top = Bodies.rectangle( width / 2, size / -2, width, size, { isStatic: true }),
         left = Bodies.rectangle(size / -2, height / 2, size, height, { isStatic: true }),
         right = Bodies.rectangle(width + size / 2, height / 2, size, height, { isStatic: true }),
         bottom = Bodies.rectangle(width / 2, height + size / 2, width, size, { isStatic: true });
 
-    return  [top, bottom, right, left];
+    World.add(this.engine.world, [top, bottom, right, left]);
+  }
+
+  initMouse() {
+    var mouse = Mouse.create(this.render.canvas);
+
+    let mouseConstraint = MouseConstraint.create(this.engine, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.005,
+            render: {
+                visible: true
+            }
+        }
+    });
+      
+    this.render.mouse = mouse;
+    World.add(this.engine.world, mouseConstraint);
   }
 
   start() {
     Engine.clear(this.engine)
 
-    var mouse = Mouse.create(this.render.canvas),
-    mouseConstraint = MouseConstraint.create(this.engine, {
-        mouse: mouse,
-        constraint: {
-            stiffness: 0.005,
-            render: {
-                visible: false
-            }
-        }
-    });
-
-    World.add(this.engine.world, mouseConstraint);
-
-    this.render.mouse = mouse;
-    
     this.createWalls();
+    this.initMouse();
+    
 
+    this.engine.world.gravity.y = 0;
     // create two boxes and a ground
     var boxA = Bodies.rectangle(400, 200, 80, 80);
     var boxB = Bodies.rectangle(450, 50, 80, 80);
 
-    World.add(this.engine.world, this.createWalls());
     World.add(this.engine.world, [boxA, boxB]);
 
 
