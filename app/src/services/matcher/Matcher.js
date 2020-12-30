58c43796e592272af991cefaf4808d8492ce8732
x4e554c4c <wanderingdwarf03@gmail.com>
2020-12-30 13:50:20 +0200

    layout change (Matcher)

--- app/src/services/matcher/Matcher.js
+++ app/src/services/matcher/Matcher.js
@@ -1,8 +1,10 @@
-import {Engine, Render, World, Bodies, Mouse, MouseConstraint} from 'matter-js'
+import {Engine, Render, World, Bodies, Events, Mouse, MouseConstraint} from 'matter-js'
 import IrregularVerbs from '@services/matcher/IrregularVerbs'
+import Collisions from '@services/matcher/Collisions'
 import Config from '@services/matcher/Config'
 import Bricks from '@services/matcher/Bricks'
-import Events from '@services/matcher/Events'
+import Groups from '@services/matcher/Groups'
+
 
 
 export default class Matcher {
@@ -65,9 +67,47 @@ export default class Matcher {
     World.add(this.world, mouseConstraint);
   }
 
+
+  collisionStart(event) {
+    for (let pair of event.pairs) {
+      let second = pair.bodyA;
+      let first = pair.bodyB;
+
+      if (first.label === 'brick' && second.label === 'brick') {
+        if (!!!Groups.merge(first, second, this.world))
+          Collisions.collideBricks(first, second);
+        return;
+      }
+
+      if (first.label === 'group' && second.label === 'brick') {
+        if (!!!Groups.append(first, second))
+          Collisions.collideGroup(first, second);
+        return
+      }
+
+      if (second.label === 'group' && first.label === 'brick') {
+        if (!!!Groups.append(second, first))
+          Collisions.collideGroup(second, first);
+        return
+      }
+    }
+  }
+
+  afterRender(event) {
+    Bricks.drawText(this.render)
+  }
+
+  bind() {
+    Events.on(this.engine, 'collisionStart', 
+      (event) => this.collisionStart(event));
+      
+    Events.on(this.render, 'afterRender',
+      (event) => this.afterRender(event));
+  }
+
   start() {
     Engine.clear(this.engine);
-    Events.bind(this.engine, this.render);
+    this.bind()
 
     this.world = this.engine.world;
     this.world.gravity.y = 0;
