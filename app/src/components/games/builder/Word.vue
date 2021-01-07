f4e6f56081efbfc7654c2c6e64a817f2e7a6015f
x4e554c4c <wanderingdwarf03@gmail.com>
2021-01-07 21:41:53 +0200

    layout (Builder)

---
+++ app/src/components/games/builder/Word.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="builder__brick">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: { type: String, default: '' }
+  },
+
+  data() {
+    return {
+      dragging: false,
+      draggable: false
+    }
+  },
+
+  computed: {
+    parent() {
+      return this.$el.offsetParent;
+    }
+  },
+
+  mounted() {
+    this.move = this.move.bind(this);
+  },
+
+  methods: {
+    startDrag() {
+      this.parent.addEventListener('move', this.move);
+    },
+
+    move() {
+
+    },
+
+    stopDrag() {
+      this.parent.removeEventListerner('move', this.move);
+    }
+  }
+}
+</script>
