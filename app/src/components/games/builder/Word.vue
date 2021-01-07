adb9e1ce898f408522bf62c235e6c7dd3b12b630
x4e554c4c <wanderingdwarf03@gmail.com>
2021-01-07 22:07:34 +0200

    word drag (Builder)

--- app/src/components/games/builder/Word.vue
+++ app/src/components/games/builder/Word.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="builder__brick">
+  <div 
+    class="builder__brick"
+    :style="{
+      'position': position,
+      'left': left,
+      'top': top}"
+
+    @mousedown="startDrag">
+
     {{ text }}
   </div>
 </template>
@@ -13,31 +21,77 @@ export default {
   data() {
     return {
       dragging: false,
-      draggable: false
+      draggable: false,
+
+      coords: {
+        x: 0,
+        y: 0
+      },
+
+      offset: {
+        x: 0,
+        y: 0
+      }
     }
   },
 
   computed: {
     parent() {
       return this.$el.offsetParent;
+    },
+
+    top() {
+      return (this.coords.y - this.offset.y) + 'px'
+    },
+
+    left() {
+      return (this.coords.x - this.offset.x) + 'px'
+    },
+
+    position() {
+      return this.dragging ? 'absolute' : '';
     }
   },
 
   mounted() {
     this.move = this.move.bind(this);
+    this.stopDrag = this.stopDrag.bind(this);
   },
 
   methods: {
-    startDrag() {
-      this.parent.addEventListener('move', this.move);
+    comutedCoords(parent, event) {
+      let offset = 
+        parent.getBoundingClientRect();
+
+      let position = {
+        x: event.clientX - offset.left,
+        y: event.clientY - offset.top
+      }
+
+      return position;
+    },
+    
+    startDrag(event) {
+      this.offset = this.comutedCoords(this.$el, event);
+      this.coords = this.comutedCoords(this.parent, event);
+
+      this.dragging = true;
+
+      this.parent.addEventListener('mousemove', this.move);
+      this.parent.addEventListener('mouseup', this.stopDrag);
+      this.parent.addEventListener('mouseleave', this.stopDrag);
     },
 
-    move() {
-
+    move(event) {
+      this.coords = 
+        this.comutedCoords(this.parent, event);
     },
 
     stopDrag() {
-      this.parent.removeEventListerner('move', this.move);
+      this.dragging = false;
+      this.parent.removeEventListener('mousemove', this.move);
+      this.parent.removeEventListener('mouseup', this.stopDrag);
+      this.parent.removeEventListener('mouseleave', this.stopDrag);
     }
   }
 }
