240544079e107dc3d70cfc1be98dddbdfc8284fd
x4e554c4c <wanderingdwarf03@gmail.com>
2021-01-09 00:07:05 +0200

    pool word return (Builder)

--- app/src/components/games/builder/Pool.vue
+++ app/src/components/games/builder/Pool.vue
@@ -1,26 +1,36 @@
 <template>
   <div class="builder__pool">
 
-    <word 
-      v-for="(word, index) in words"
+    <div 
+      v-for="(brick, index) in bricks"
       :key="index"
 
-      :text="word"
-      @drag="(options) => drag(index, options)"/>
+      ref="words"
+      class="builder__brick"
+      
+      :style="{
+        'opacity': computeOpacity(brick),
+        'height': computeHeight(brick),
+        'width': computeWidth(brick)}"
+
+      @mousedown="(event) => drag(index, event)">
+
+      {{ brick.word }}
+    </div>
 
   </div>
 </template>
 
 <script>
-import Word from '@components/games/builder/Word'
-
 export default {
-  components: {
-    Word
-  },
-  
   props: {
-    words: { type: Array, default: () => [] }
+    words: { type: Array, default: () => [] },
+  },
+
+  data() {
+    return  {
+      bricks: []
+    }
   },
 
   inject: ['$dragger'],
@@ -33,23 +43,77 @@ export default {
 
   mounted() {
     this.dragger.remove = this.put.bind(this);
+    this.parseWords();
   },
 
   methods: {
-    remove(index) {
-      this.words.splice(index, 1);
+    parseWords() {
+      let bricks = [];
+      for (let word of this.words) {
+        let brick = {
+          word, disabled: false
+        };
+
+        bricks.push(brick);
+      }
+
+      this.bricks = bricks;
     },
 
-    drag(index, options) {
-      let {target, event} = options;
+    remove(index) {
+      this.bricks.splice(index, 1);
+    },
+
+    drag(index, event) {
+      let target = {
+        word: this.bricks[index].word,
+        el: this.$refs.words[index]
+      };
 
       this.remove(index);
       this.dragger.drag(target, event);
     },
 
-    put(target) {
-      target.el.remove();
-      this.words.push(target.word)
+    async put(target) {
+      let brick = {
+        disabled: true,
+      }
+
+      brick.word = target.word;
+      brick.width = target.el.offsetWidth;
+      brick.height = target.el.offsetHeight;
+      let index = this.bricks.push(brick) - 1;
+
+      // Waiting for dom update
+      await this.$nextTick();
+
+      let el = this.$refs.words[index];
+      let dummy = target.el;
+
+      dummy.style.transition = 'left 1s, top 1s';
+      dummy.style.left = el.offsetLeft + 'px';
+      dummy.style.top = el.offsetTop + 'px';
+
+      // Waiting for animation
+      await new Promise(resolve => setTimeout(resolve, 1000))
+
+      dummy.remove();
+      brick.disabled = false;
+    },
+
+    computeWidth(brick) {
+      return brick.disabled ? 
+        brick.width + 'px' : 'auto';
+    },
+
+    computeHeight(brick) {
+      return brick.disabled ? 
+        brick.height + 'px' : 'auto';
+    },
+
+    computeOpacity(brick) {
+      return brick.disabled ? 
+        '0' : '1';
     }
   }
 }
