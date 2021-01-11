74fbcfde5c18bc78aec7d5e43f3499a4639a6496
x4e554c4c <wanderingdwarf03@gmail.com>
2021-01-11 04:02:58 +0200

    smooth rgb (Games)

--- app/src/pages/games/GamesPool.vue
+++ app/src/pages/games/GamesPool.vue
@@ -5,8 +5,7 @@
       :key="index"
       @click="select(game)"
       :style="{
-        'background-color': game.color ,
-        'color': 'white' }">
+        'background-color': computeColor(index) }">
 
       {{ game.name }}
     </button>
@@ -19,18 +18,41 @@ function randomColor() {
   return '#' + Math.floor(Math.random() * Math.pow(16, 6)).toString(16).padStart(6, '0')
 }
 
+function randomRgb() {
+  return [1, 2, 3].map(() => Math.floor(Math.random() * 250));
+}
+
 export default {
   data() {
     return {
       games: [
-        { name: 'Whiteboard', component: '@componetns/games/Whiteboard', color: randomColor()}
-      ]
+        { name: 'Whiteboard', component: '@componetns/games/Whiteboard', color: randomColor()},
+        { name: 'Whiteboard', component: '@componetns/games/Whiteboard', color: randomColor()},
+        { name: 'Whiteboard', component: '@componetns/games/Whiteboard', color: randomColor()},
+        { name: 'Whiteboard', component: '@componetns/games/Whiteboard', color: randomColor()},
+        { name: 'Whiteboard', component: '@componetns/games/Whiteboard', color: randomColor()},
+        { name: 'Whiteboard', component: '@componetns/games/Whiteboard', color: randomColor()},
+        { name: 'Whiteboard', component: '@componetns/games/Whiteboard', color: randomColor()},
+        { name: 'Whiteboard', component: '@componetns/games/Whiteboard', color: randomColor()},
+      ],
+
+      color: randomRgb()
     }
   },
 
   methods: {
     select(game) {
       this.$emit('select', game);
+    },
+
+    computeColor(index) {
+      const STEP = 8;
+
+      let [r, g, b] = this.color;
+
+      let k = (this.games.length - index - 1) * STEP
+
+      return `rgb(${r - k}, ${g - k}, ${b - k})`
     }
   }
 }
@@ -42,11 +64,12 @@ div
   padding: 20px
 
 button
+  margin: 3px 6px
   height: 40px
   font-size: 18px
   border-radius: 20px
   background: #f0f0f0
-  padding: 0 10px
-  color: #77778e
+  padding: 0 10px 
+  color: white
 
 </style>
