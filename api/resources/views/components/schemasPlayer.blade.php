67b092f670b560e9008222a8c98558dc4e2245f4
x4e554c4c <wanderingdwarf03@gmail.com>
2021-01-20 23:57:21 +0200

    removed blade

--- api/resources/views/components/schemasPlayer.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="player">
-  <div class="player__rationed">
-    <div class="player__image"></div>
-    <div class="player__overlay">
-      <object class="player__overlay-image" data="{{ asset("img/svg/image-overlay.svg") }}"
-        type="image/svg+xml"></object>
-    </div>
-  </div>
-</div>
