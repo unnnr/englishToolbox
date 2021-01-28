010579d576703e08d838c89f104dcfbd799dce71
x4e554c4c <wanderingdwarf03@gmail.com>
2021-01-28 09:22:55 +0200

    recovery fixes

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Services\Auth\VerificationService;
+use App\Services\Auth\RecoveryService;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +26,15 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function() {
+            
+            (new VerificationService())
+                ->clearExceeded();
+
+            (new RecoveryService)
+                ->clearExceeded();
+
+        })->everyMinute();
     }
 
     /**
